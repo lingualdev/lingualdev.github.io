5f93bfd5f69d8f8a609ffb8070d8a76472e8d528
busypeoples <busypeoples@gmail.com>
2024-06-19 00:35:34 +0200

    Update meta description

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,10 +2,10 @@ module.exports = {
 	title: "Lingual",
 	url: "https://www.lingual.dev/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Improving the i18n developer experience",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/",
+		name: "",
+		email: "",
+		url: "https://lingual.dev",
 	},
 };
