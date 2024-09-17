7d64d93a5f7e536e8a66c9b8885a26dacbd19de0
Stefan Oestreicher <oestef@gmail.com>
2024-09-17 16:05:12 +0200

    Implement landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./themes/lingual/layouts/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['"Source Serif Pro"'],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
